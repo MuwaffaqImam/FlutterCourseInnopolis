f71e3eb6893dd1e6ab7798d60fbef4ae6c967a9f
Muwaffaq Imam <muwaffaqimam@gmail.com>
2022-01-23 21:45:51 +0300

    update templates

--- lib/templates/FlutterStarterCode.dart
+++ lib/templates/FlutterStarterCode.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+void main() => runApp(MyApp());
+
 /// this is your APP Main screen configuration
 class MyApp extends StatelessWidget {
   MyApp({Key? key}) : super(key: key);
