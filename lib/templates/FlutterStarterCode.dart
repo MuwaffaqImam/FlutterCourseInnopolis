c9f7d263a42f6e3e91549e5d23b2e388122c4fff
Muwaffaq Imam <muwaffaqimam@gmail.com>
2022-01-23 14:22:32 +0300

    init code

---
+++ lib/templates/FlutterStarterCode.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  
+}
+
+/// this is your APP Main screen configuration
+class MyApp extends StatelessWidget {
+  MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyHomePage(),
+    );
+  }
+}
+
+/// this is a template to start building a UI
+/// to start adding any UI you want change what comes after the [ body: ] tag below
+class MyHomePage extends StatelessWidget {
+  MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xfff4f4f4),
+      /*******************--[focus here 🧐]--*******************/
+      appBar: AppBar(
+        leading: const Icon(Icons.android_sharp),
+        title: const Text('App Title'),
+        backgroundColor: Colors.teal,
+        elevation: 4,
+      ),
+      body: myWidget(),
+      /*******************--[focus here 🧐]--*******************/
+    );
+  }
+
+  Widget myWidget() {
+    return Container(
+      padding: const EdgeInsets.all(20),
+      color: Colors.blueAccent,
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: const <Widget>[
+          /*******************--[focus here 🧐]--*******************/
+          Text(
+            'Welcome to the Course',
+          ),
+          /*******************--[focus here 🧐]--*******************/
+        ],
+      ),
+    );
+  }
+}
