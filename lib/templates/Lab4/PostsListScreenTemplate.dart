8928f8e434eda7c36f96de888e8e1b361e8922c6
Muwaffaq Imam <muwaffaqimam@gmail.com>
2022-03-13 15:11:28 +0300

    ADDING templates

--- lib/templates/Lab4/PostsListScreenTemplate.dart
+++ lib/templates/Lab4/PostsListScreenTemplate.dart
@@ -3,26 +3,6 @@ import 'package:courses_codes/Labs/Lab4/AddPostScreen.dart';
 import 'package:courses_codes/templates/Lab4/AddPostScreenTemplate.dart';
 import 'package:flutter/material.dart';
 
- // [2#] Source Code كيفية التعامل مع الدورة
- // [3#]  Text كيفية التعامل مع النصوص
- // [4#]  Icons كيفية التعامل مع الأيقونات
- // [5#]  Buttons الأزرار
- // [6#]  Images الصور
- // [7#]  Container الحافظات
- // [8#]  Center & padding الحافظات
- // [9#]  Row الصفوف
- // [10#]  Column العواميد
- // [11#]  Expanded & Flex
- // [12#]  Scroll
- // [13#]  Stack
- // [14#]  Wrap
- // [15#]  Aspect Ratio حافظات
- // [16#]  UnderStanding Flutter  إفهم الكود
- // [17#]  Scaffold and Material App
- // [18#]  Introduction to DART مفدمة في دارت
- // [19#] Animated Container شرح
-
-
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
