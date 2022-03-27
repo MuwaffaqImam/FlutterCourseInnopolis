4eb25060e1c9cf28be950f069ba8773fc6d02ba9
Muwaffaq Imam <muwaffaqimam@gmail.com>
2022-03-27 10:50:40 +0300

    ADDING Firebase

--- lib/Lecture12&13-Firebase/ArtGallaryList.dart
+++ lib/Lecture12&13-Firebase/ArtGallaryList.dart
@@ -6,7 +6,7 @@ import 'Art.dart';
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp().then((value) => print(value.options.projectId));
   runApp(MyApp());
 }
 
